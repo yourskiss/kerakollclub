c6fe7602699f24d115994f40311509eadac38a5b
yourskiss <ksingh1817@gmail.com>
2024-04-10 00:47:40 +0530

    final

--- src/components/app/DashboardComponent.js
+++ src/components/app/DashboardComponent.js
@@ -7,6 +7,9 @@ import {  isUserToken } from "@/config/userauth";
 import Link from 'next/link';
 import TotalrewardpointsComponent from '../shared/TotalrewardpointsComponent';
 import CountUp from 'react-countup';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const DashboardComponent = () => {
     
@@ -20,50 +23,89 @@ const DashboardComponent = () => {
   }, [isUT]);
 
 
+
+  var settingsDashboard = {
+    dots: true,
+    arrows:false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+     centerPadding: '10px'
+  };
+
   return (
   <>
     <HeaderComponent />
-    <div className="screenmain" > 
+    <div className="screenmain screendashboard"> 
       <div className="screencontainer">
    
-          <div className="dashboard_head">
-              <h2>
-                  PRODUCT CATEGORIES
-                  <span>Earn reward points everytime you purchase!</span>
-              </h2>
-          </div>
-
-          <div className="dashboard_single_pro">
-              <aside>
-                <Image src="/assets/images/dashboard_single_pro.png" width={100} height={100} alt="product" quality={100} />
-              </aside>
-              <h2>
-                  <span>Learn more</span>
-                  <Image src="/assets/images/arrows.png" width={100} height={100} alt="product" quality={100} />
-              </h2>
-          </div>
-
- 
           <div className="dashboard_earned_point">
                 <h2>You’ve earned</h2>
+                <p><CountUp duration={2} start={0}  delay={1}  end={rewardspoints} /></p>
                 <p>
-                    <CountUp duration={2} start={0}  delay={1}  end={rewardspoints} /> 
+                    <Image src="/assets/images/three-start.png" width={100} height={88} alt="star" quality={100} />
                     <em>reward<br />points</em>
                 </p>
           </div>
 
           <div className="dashboard_content">
               <section>
-                    <aside><Link href="/scanqrcode"><Image src="/assets/images/dash-qr.png" width={100} height={100} alt="qr" quality={100} /></Link></aside>
+                <Link href="/scanqrcode">
+                    <aside><Image className='dashboard_scan_img' src="/assets/images/dash-qr.png" width={100} height={100} alt="qr" quality={100} /></aside>
                     <h2>Scan QR Code</h2>
                     <p>FOR YOUR KERAKOLL PRODUCTS</p>
+                </Link>
               </section>
-              <section>
-                    <aside><Link href="/"><Image src="/assets/images/redeempoints.png" width={99} height={115} alt="redeempoints" quality={100} /></Link></aside>
+              <section className='dashboard_redeempointbg'>
+                <Link href="/">
+                    <aside><Image src="/assets/images/redeempoints.png" width={99} height={115} alt="redeempoints" quality={100} /></aside>
+                    <h5><CountUp duration={2} start={0}  delay={1}  end={rewardspoints} /> <em>pt</em></h5>
                     <h2>Redeem Points</h2>
                     <p>IN YOUR CLUB WALLET</p>
+                </Link>
               </section>
           </div>
+        </div>
+    </div>
+    <div className="screenmain screendashboardbottom"> 
+        <div className="screencontainer">
+
+            <div className="dashboard_products">
+                <h2>PRODUCT CATEGORIES</h2>
+                <Slider className="dashboard_slider" {...settingsDashboard}>
+                    <div className="db_item">
+                        <aside><Image src="/assets/images/products/img.png" width={500} height={500} alt="product" quality={99} /></aside>
+                        <h2>
+                            <span>Learn more</span>
+                            <Image src="/assets/images/arrows.png" width={57} height={18} alt="product" quality={99} />
+                        </h2>
+                    </div>
+                    <div className="db_item">
+                        <aside><Image src="/assets/images/products/img.png" width={500} height={500} alt="product" quality={99} /></aside>
+                        <h2>
+                            <span>Learn more</span>
+                            <Image src="/assets/images/arrows.png" width={57} height={18} alt="product" quality={99} />
+                        </h2>
+                    </div>
+                    <div className="db_item">
+                        <aside><Image src="/assets/images/products/img.png" width={500} height={500} alt="product" quality={99} /></aside>
+                        <h2>
+                            <span>Learn more</span>
+                            <Image src="/assets/images/arrows.png" width={57} height={18} alt="product" quality={99} />
+                        </h2>
+                    </div>
+                    <div className="db_item">
+                        <aside><Image src="/assets/images/products/img.png" width={500} height={500} alt="product" quality={99} /></aside>
+                        <h2>
+                            <span>Learn more</span>
+                            <Image src="/assets/images/arrows.png" width={57} height={18} alt="product" quality={99} />
+                        </h2>
+                    </div>                
+                </Slider>
+            </div>
+
           
       </div>
     </div> 
